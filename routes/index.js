ac2f1d42ef9ca82a08344eb32af3c14b6066926c
Kristy & Rena Goldsmith <pair+kristy-mj.rena-deane@devbootcamp.com>
2016-06-23 11:25:27 +1200

    completed showing map using api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var dotenv = require('dotenv')
+
+
+dotenv.load()
+
+var googleMapsKey = process.env.GOOGLE_MAP_BROWSER_KEY
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Where you at?", googleMap: "https://maps.googleapis.com/maps/api/js?key=" + googleMapsKey + "&callback=initMap" });
+});
+
+router.get('/', function(req, res, next) {
+  var google
 });
 
 module.exports = router;
