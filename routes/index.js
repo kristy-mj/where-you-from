ed54e48bca8f3d85d7191e2705682be7c0d2b2a1
Kristy & Rena Goldsmith <pair+kristy-mj.rena-deane@devbootcamp.com>
2016-06-23 16:03:35 +1200

    add quiz page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var dotenv = require('dotenv')
-var dummyData = require('../database/db.json')
-
 
 dotenv.load()
 
-var googleMapsKey = process.env.GOOGLE_MAP_BROWSER_KEY
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Who are you?");
+  res.redirect('/startquiz')
+})
+
+
+router.get('/startquiz', function(req, res, next) {
+  res.render('quiz', { title: "What you like?"});
 });
 
 module.exports = router;
